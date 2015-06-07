71185dfa23ee5fdb4b5779a468686d3d4176fa18
Muhammad Zuhri Hanifullah <zuhri2395@gmail.com>
2015-06-08 00:03:22 +0700

    Data handler

---
+++ Data Structure/Final Project/src/project/data/Kids.java
@@ -0,0 +1,13 @@
+package project.data;
+
+public class Kids {
+    Kids next;
+    String name;
+    int age, status;
+    
+    public Kids(String name, int age, int status) {
+	this.name = name;
+	this.age = age;
+	this.status = status;
+    }
+}
